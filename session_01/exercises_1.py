6a3a1c14d6acac6e1aa22221c3e7eb61cc2ac6c9
Replit user <>
2023-06-15 15:19:18 +0000

    session 1 exercises

--- session_01/exercises_1.py
+++ session_01/exercises_1.py
@@ -10,25 +10,25 @@
 
 
 ## Section A
-# 1. Write code that prints ‘Hello world’.
+# # 1. Write code that prints ‘Hello world’.
+# print("Hello World!")
 
 
-
-# 2. Print the numbers 1 to 5 on a single line.
-
-
-
-# 3. Write a script where ‘Hello’ and ‘World’ are printed on two separate lines.
+# # 2. Print the numbers 1 to 5 on a single line.
+# print(1,2,3,4,5)
 
 
+# # 3. Write a script where ‘Hello’ and ‘World’ are printed on two separate lines.
+# print("Hello\nWorld")
 
-# 4. Write a script that prints a list of names, tabbed on separate lines, e.g.
-#     ```My List of Names:
-#             Alice
-#             Bob
-#             Charlie
-#     ```
 
+# # 4. Write a script that prints a list of names, tabbed on separate lines, e.g.
+# #     ```My List of Names:
+# #             Alice
+# #             Bob
+# #             Charlie
+# #     ```
+# print("My list of names\n\tJacob\n\tEllie\n\tPersephone\n\tBrian")
 
 
 
@@ -36,23 +36,26 @@
 
 ## Section B
 # 1. Write code that prints the value of 2 + 2.
-
+print(2+2)
+print(" ")
 
 
 # 2. Write code that prints the value of 5.7 subtracted from 3.4.
-
-
+print(5.7-3.4)
+print(" ")
 
 # 3. Write code that prints the value of 8 multiplied by 7.
-
-
+print(8*7)
+print(" ")
 
 # 4. Write code that prints the value of 144 divided by 12.
-
+print(144/12)
+print(" ")
 
 
 # 5. Write code that prints the value of the remainder of 67 divided by 12.
-
-
+print(67%12)
+print(" ")
 
 # 6. Write code that finds the value of 20 from `4 - 2 * 6 / 3 * 5`. Hint: you might need brackets.
+print((((4-2)*6)/3)*5)
